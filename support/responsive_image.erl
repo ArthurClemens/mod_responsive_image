4904fa57416cf0efd8989fe5fd63840e0a4546b3
Arthur Clemens <arthurclemens@gmail.com>
2015-02-01 18:58:31 +0100

    Create module to handle data processing

--- support/responsive_image.erl
+++ support/responsive_image.erl
@@ -1,27 +1,12 @@
--module(service_responsive_image_get_url).
+-module(responsive_image).
 -author("Arthur Clemens").
 
--svc_title("Generate image data").
--svc_needauth(false).
-
--export([process_get/2]).
+-export([get_url/2]).
 
 -include_lib("zotonic.hrl").
 
-process_get(_ReqData, Context) ->
-    Json = z_context:get_q("request", Context),
-    Result = case Json of
-        undefined -> {error, "no json data"};
-        _ -> process_json_data(Json, Context)
-    end,
-    case Result of
-        {ok, Url} -> {struct, [{url, Url}]};
-        {error, Error} -> {struct, [{error, Error}]}
-    end.
 
-        
-process_json_data(Json, Context) ->
-    {struct, Data} = mochijson2:decode(Json),
+get_url(Data, Context) ->
     case check_required(Data) of
         {error, E} -> {error, E};
         ok ->
@@ -133,4 +118,3 @@ add_property(Id, Key, Data, Context) when
     end;
 
 add_property(_Id, _Key, _Data, _Context) -> [].
-    
